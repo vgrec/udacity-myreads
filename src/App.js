156bc0eaeed8ce7158bd2dcd3aba718787091d2f
Veaceslav Grec <grec.vs@gmail.com>
2023-12-20 20:01:29 +0100

    fetch books from backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchPage from "./components/SearchPage";
 import BooksPage from "./components/BooksPage";
 import { Route, Routes } from "react-router-dom";
+import { getAll, search } from "./BooksAPI";
 
-function App() {
-  const [shelfs, setShelfs] = useState([
+/*
     {
       title: "Currently Reading",
       books: [
@@ -21,42 +21,62 @@ function App() {
         }
       ]
     },
-    {
-      title: "Want to Read",
-      books: [
-        {
-          bookTitle: "1776",
-          bookAuthor: "David McCullough",
-          bookImage: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-        },
-        {
-          bookTitle: "Harry Potter and the Sorcerer's Stone",
-          bookAuthor: "J.K. Rowling",
-          bookImage: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-        }
-      ]
-    },
-    {
-      title: "Read",
-      books: [
-        {
-          bookTitle: "The Hobbit",
-          bookAuthor: "J.R.R. Tolkien",
-          bookImage: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-        },
-        {
-          bookTitle: "Oh, the Places You'll Go!",
-          bookAuthor: "Seuss",
-          bookImage: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
-        },
-        {
-          bookTitle: "The Adventures of Tom Sawyer",
-          bookAuthor: "Mark Twain",
-          bookImage: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
-        }
-      ]
+    */
+
+function App() {
+  const [shelfs, setShelfs] = useState([])
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      const res = await getAll();
+      console.log(res);
+
+      return res;
     }
-  ])
+
+    fetchAllBooks().then(remoteShelves => {
+      const shelfData = remoteShelves.map(remoteShelf => ({
+        title: remoteShelf.shelf,
+        bookTitle: remoteShelf.title,
+        bookAuthor: remoteShelf.authors.join(", "),
+        bookImage: remoteShelf.imageLinks.thumbnail
+      }));
+
+      let dictionary = {};
+      shelfData.forEach(shelf => {
+        if (dictionary[shelf.title]) {
+          dictionary[shelf.title].push({
+            bookTitle: shelf.bookTitle,
+            bookAuthor: shelf.bookAuthor,
+            bookImage: shelf.bookImage
+          });
+        } else {
+          dictionary[shelf.title] = [{
+            bookTitle: shelf.bookTitle,
+            bookAuthor: shelf.bookAuthor,
+            bookImage: shelf.bookImage
+          }];
+        }
+      });
+
+      console.log(dictionary);
+
+      const shelfs = Object.keys(dictionary).map(key => ({
+        title: key,
+        books: dictionary[key]
+      }));
+
+
+      setShelfs(shelfs);
+    });
+
+    // const searchBooks = async (query, maxResults) => {
+    //   const res = await search(query, maxResults);
+    //   console.log(res);
+    // }
+    // searchBooks("Linux", 20);
+
+  }, [])
 
   return (
     <div className="app">
